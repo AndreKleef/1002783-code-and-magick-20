81542ae2d697b2739c60dcd758745e17501081c2
AndreKleef <kleef001@gmail.com>
2020-07-14 17:13:19 +0400

    module6-task1 v4

--- js/backend.js
+++ js/backend.js
@@ -6,9 +6,9 @@
   var statusCode = {
     OK: 200
   };
-  var REQUEST_TIMEOUT_IN_MS = 10000;
+  var REQUEST_TIMEOUT_IN_MS = 5000;
 
-  var load = function (onSuccess, onError) {
+  var createXhr = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
@@ -28,31 +28,19 @@
     });
 
     xhr.timeout = REQUEST_TIMEOUT_IN_MS;
+
+    return xhr;
+  };
+
+  var load = function (onSuccess, onError) {
+    var xhr = createXhr(onSuccess, onError);
 
     xhr.open('GET', URL_LOAD);
     xhr.send();
   };
 
   var upload = function (data, onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === statusCode.OK) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = REQUEST_TIMEOUT_IN_MS;
+    var xhr = createXhr(onSuccess, onError);
 
     xhr.open('POST', URL_UPLOAD);
     xhr.send(data);
